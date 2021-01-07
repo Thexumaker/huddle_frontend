d4558eac078d7586cb110a0282506f6383a5e105
David <thexumaker@berkeley.edu>
2021-01-07 15:01:09 -0800

    Register

--- components/signup/signupbox.js
+++ components/signup/signupbox.js
@@ -1,23 +1,37 @@
 import styles from './signupbox.module.css'
-import { useState } from 'react';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import Router from 'next/router'
+
+
+
+
 const SignInBox = (props) => {
-    const [userName, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const handleUserName = event => {
-        setName(event.target.value);
-        };
-        const handlePassword = event => {
-            setPassword(event.target.value);
-            };
+    const { register, handleSubmit } = useForm();
+    const onSubmit = async (data) => {
+        alert(JSON.stringify(data))
+        const res = await axios.post('https://huddledatingapp.herokuapp.com/user/create',
+        {
+            email: data.userName,
+            password: data.userpassword
+
+        })
+        console.log(res)
+        alert(JSON.stringify(res.data))
+        Router.push('/')
+
+
+    }
+    
 
     return (
     <div className={styles.body}>
     <div className={styles.main}>
-    <p className={styles.sign} align="center">Register</p>
-    <form className={styles.form1}>
-      <input className={styles.un} type="text" align="center" value={userName} onChange={handleUserName} placeholder="Username" />
-      <input className={styles.pass} type="password" value={password} align="center" onChange={handlePassword} placeholder="Password" />
-      <a className={`${styles.submit} ${styles.a}`} align="center">Register</a>
+    <p className={styles.sign} align="center">Sign in</p>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form1}>
+      <input className={styles.un} name= 'userName' type="text" align="center" ref={register} placeholder="Username" />
+      <input className={styles.pass} name= 'userpassword' type="password" ref={register}  align="center"  placeholder="Password" />
+      <input type='submit' value="Sign in" className={`${styles.submit} ${styles.a}`} align="center" />
       <p className={styles.forgot} align="center"><a href="#">Forgot Password?</a></p>
             
     </form>         
