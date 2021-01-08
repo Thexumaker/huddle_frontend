10e686b4162c4f143a3d7ec0ff9ab514cb66230b
David <thexumaker@berkeley.edu>
2021-01-08 01:24:40 -0800

    da

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,7 @@
 import styles from '../styles/Home.module.css'
 import BottomNav from '../components/DatingContent/bootomNav/bottomNav'
 import UserImage from '../components/DatingContent/mainPage/UserImage'
-import UserName from '../components/DatingContent/mainPage/userName'
+import UserName from '../components/DatingContent/mainPage/UserName'
 
 export default function Home() {
     return (
